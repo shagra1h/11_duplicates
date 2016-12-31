5c93cee1077931c86807242b3b1972efc36b20cc
shagra1h <achkael.serg@yandex.ru>
2016-12-31 20:55:49 +0300

    Программа для поиска дубликатов файлов

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,54 @@
+import os
+import os.path
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+def check_path_exsistence(target_path):
+    if os.path.exists(target_path):
+        return target_path
 
+
+def group_files_by_size(dir_name):
+    files_grouped_by_size = {}
+    for path, dirs, files in os.walk(dir_name):
+        for filename in files:
+            full_path = os.path.join(path, filename)
+            filesize = os.path.getsize(full_path)
+            if filesize in files_grouped_by_size:
+                files_grouped_by_size[filesize].append(full_path)
+            else:
+                files_grouped_by_size[filesize] = [full_path]
+    return files_grouped_by_size
+
+
+def eliminate_unique_files(files_grouped_by_size):
+    amount_of_duplicates = 1
+    processed_files = {}
+    for filesize, filelist in files_grouped_by_size.items():
+        for file1 in filelist:
+            for file2 in filelist:
+                if os.path.split(file1)[1] == os.path.split(file2)[1] and file1 != file2:
+                        amount_of_duplicates += 1
+            if amount_of_duplicates != 1:
+                if filesize in processed_files:
+                    processed_files[filesize].append(file1)
+                else:
+                    processed_files[filesize] = [file1]
+            amount_of_duplicates = 1
+    return processed_files
 
 if __name__ == '__main__':
-    pass
+    while True:
+        target_directory = input("Enter directory to find duples: \n")
+        if check_path_exsistence(target_directory) is None:
+            print("Incorrect path! Try again!")
+        else:
+            break
+    print("Analyzing files... \n")
+    files_in_target_dir = group_files_by_size(target_directory)
+    files_in_target_dir = eliminate_unique_files(files_in_target_dir)
+    if files_in_target_dir:
+        for filesize, filelist in files_in_target_dir.items():
+            print("Found duplicates, size =", filesize)
+            print('\n'.join(filelist), '\n')
+    else:
+        print("No duplicates found!")
