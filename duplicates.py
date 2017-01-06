073e122d3505c3706d5f56e8e527f595b6083cd2
shagra1h <achkael.serg@yandex.ru>
2017-01-06 15:38:36 +0300

    Упрощена функция eliminate_unique_files()

--- duplicates.py
+++ duplicates.py
@@ -20,20 +20,24 @@ def group_files_by_size(dir_name):
     return files_grouped_by_size
 
 
+def count_duplicates(filelist, aim_file):
+    duplicates = 1
+    for target in filelist:
+        if os.path.split(aim_file)[1] == os.path.split(target)[1] and target != aim_file:
+            duplicates += 1
+    return duplicates
+
 def eliminate_unique_files(files_grouped_by_size):
     amount_of_duplicates = 1
     processed_files = {}
     for filesize, filelist in files_grouped_by_size.items():
         for file1 in filelist:
-            for file2 in filelist:
-                if os.path.split(file1)[1] == os.path.split(file2)[1] and file1 != file2:
-                        amount_of_duplicates += 1
+            amount_of_duplicates = count_duplicates(filelist, file1)
             if amount_of_duplicates != 1:
                 if filesize in processed_files:
                     processed_files[filesize].append(file1)
                 else:
                     processed_files[filesize] = [file1]
-            amount_of_duplicates = 1
     return processed_files
 
 if __name__ == '__main__':
@@ -45,7 +49,9 @@ if __name__ == '__main__':
             break
     print("Analyzing files... \n")
     files_in_target_dir = group_files_by_size(target_directory)
+    #print(files_in_target_dir, "\n")
     files_in_target_dir = eliminate_unique_files(files_in_target_dir)
+    #print(files_in_target_dir)
     if files_in_target_dir:
         for filesize, filelist in files_in_target_dir.items():
             print("Found duplicates, size =", filesize)
